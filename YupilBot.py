fbaeb12f8ea6cb94869aed7828c8d15123a20168
mumochi <149116223+mumochi@users.noreply.github.com>
2024-05-17 21:35:51 -0400

    Update YupilBot.py
    Added DeepL translation feature

--- YupilBot.py
+++ YupilBot.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands as ac
+import deepl
 
 server_id =  # Server ID
 permitted_role = "" # Only users with this role can use the commands
@@ -7,6 +8,18 @@ permitted_role = "" # Only users with this role can use the commands
 intents = discord.Intents.default() 
 client = discord.Client(intents = intents)
 tree = ac.CommandTree(client) 
+
+# DeepL authorization
+try:
+    with open("deepl_key.txt") as keyfile:
+        auth_key = keyfile.readline()
+except FileNotFoundError:
+    print("Missing deepl_key.txt; generate an auth key and copy-paste it into deepl_key.txt in the YupilBot.py directory.")
+
+try:
+    translator = deepl.Translator(auth_key)
+except:
+    print("Invalid key - check current key or generate a new one.")
 
 # Chat command: bot sends a normal chat message to a text channel
 @tree.command(
@@ -39,6 +52,21 @@ async def dm(ctx, dm_message: str, user: discord.User):
     """Sends a DM to the indicated user."""
     await user.send(dm_message)
     await ctx.response.send_message(f"DM sent to {user.id}: " + dm_message)
+
+# Translation command using DeepL API
+@tree.command(
+        name = "translate",
+        description = "Translates text to English (EN-US) using the DeepL API.",
+        guild = discord.Object(id = server_id)
+)
+@ac.checks.has_role(permitted_role)
+@ac.describe(
+    text = "Text to translate"
+)
+async def translate(ctx, text: str):
+    """Translates text to English (EN-US) using the DeepL API."""
+    tr_text = translator.translate_text(text, target_lang = "EN-US")
+    await ctx.response.send_message(f"{text} -> " + str(tr_text) + " (EN-US)")
 
 # Sync commands
 @client.event
