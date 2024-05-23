418ba746b34b72c02e11e75810808e1d85bb4624
mumochi <149116223+mumochi@users.noreply.github.com>
2024-05-23 15:06:06 -0400

    Update YupilBot.py

--- YupilBot.py
+++ YupilBot.py
@@ -2,8 +2,8 @@ import discord
 from discord import app_commands as ac
 import deepl
 
-server_id =  # Server ID
-permitted_role = "" # Only users with this role can use the commands
+server_id = 566788769055768587 # Server ID
+permitted_role = "Yoderator" # Only users with this role can use the commands
 
 intents = discord.Intents.default() 
 client = discord.Client(intents = intents)
@@ -52,6 +52,50 @@ async def dm(ctx, dm_message: str, user: discord.User):
     """Sends a DM to the indicated user."""
     await user.send(dm_message)
     await ctx.response.send_message(f"DM sent to {user.id}: " + dm_message)
+
+# Restrict command: bot restricts user permissions to view server channels
+@tree.command(
+        name = "restrict",
+        description = "Restricts a user.",
+        guild = discord.Object(id = server_id)
+)
+@ac.checks.has_role(permitted_role)
+@ac.describe(
+    user = "User to restrict"
+)
+async def restrict(ctx, user: discord.User):
+    """Restricts a user."""
+    for channel in ctx.guild.text_channels:
+        perms = channel.overwrites_for(user)
+        perms.read_messages = False
+        await channel.set_permissions(user, overwrite = perms)
+    for vc in ctx.guild.voice_channels:
+        perms_vc = vc.overwrites_for(user)
+        perms_vc.view_channel = False
+        await vc.set_permissions(user, overwrite = perms_vc)
+    await ctx.response.send_message(f"{user} restricted.")
+
+# Unrestrict command: bot unrestricts user permissions to view server channels
+@tree.command(
+        name = "unrestrict",
+        description = "Unrestricts a user.",
+        guild = discord.Object(id = server_id)
+)
+@ac.checks.has_role(permitted_role)
+@ac.describe(
+    user = "User to unrestrict"
+)
+async def unrestrict(ctx, user: discord.User):
+    """Unrestricts a user."""
+    for channel in ctx.guild.text_channels:
+        perms = channel.overwrites_for(user)
+        perms.read_messages = None
+        await channel.set_permissions(user, overwrite = perms)
+    for vc in ctx.guild.voice_channels:
+        perms_vc = vc.overwrites_for(user)
+        perms_vc.view_channel = None
+        await vc.set_permissions(user, overwrite = perms_vc)
+    await ctx.response.send_message(f"{user} unrestricted.")
 
 # Translation command using DeepL API
 @tree.command(
