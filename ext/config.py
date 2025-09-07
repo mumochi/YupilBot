2cd185e87f2b09ff5d2f72cd18f89fda084d6170
mumochi <149116223+mumochi@users.noreply.github.com>
2025-09-06 22:22:51 -0400

    Add files via upload
    init

---
+++ ext/config.py
@@ -0,0 +1,26 @@
+# Config module to establish shared variables
+
+import os
+import configparser
+from dotenv import load_dotenv
+
+class ConfigManager:
+    def __init__(self, config_file):
+        yupil_env = os.getenv("YUPIL_ENV")
+        if yupil_env != "prod":
+            load_dotenv(".env.local")
+        else:
+            load_dotenv(".env")
+        self.server_id = os.getenv("DISCORD_SERVER_ID")
+        self.token = os.getenv("DISCORD_TOKEN")
+        # Read config info
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        config_env = config[yupil_env]
+        self.welcome_channel = int(config_env['welcome_channel'])
+        self.log_channel = int(config_env['log_channel'])
+        self.priority_log_channel = int(config_env['priority_log_channel'])
+        self.max_messages = int(config_env['cache_size'])
+        self.permitted_role = config_env['permitted_role']  # Only users with this role can use the commands
+        self.all_role = config_env['all_role'] # Cosmetic role assigned to every member upon joining
+        self.vc_role = config_env['vc_role'] # Role assigned allowing VC access
---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import logging
+import discord
+from discord.ext import commands
+from ext.config import ConfigManager
+from ext.helpers import Helpers
+
+# Set intents. Make sure that intents and server permissions match what is needed for bot functionality
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.messages = True
+
+config = ConfigManager(config_file="config.ini")
+
+# Bot setup
+# Create the setup hook which will load all extensions in the ./ext directory and sync commands
+ext_list = [ext.rstrip(".py") for ext in os.listdir("./ext") if ext.endswith(".py") and ext != "config.py" and ext != "helpers.py"]
+
+class BotClient(commands.Bot):
+    def __init__(self, *, command_prefix: str, intents: discord.Intents, max_messages: int):
+        super().__init__(command_prefix=command_prefix, intents=intents, max_messages=max_messages)
+        self.config = config
+        self.helpers = Helpers()
+
+    async def setup_hook(self):
+        for ext in ext_list:
+            await self.load_extension(f"ext.{ext}")
+        await self.tree.sync()
+        
+bot = BotClient(command_prefix='/', intents=intents, max_messages=config.max_messages)
+
+# Login and run with simple logging enabled
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+    print(f"Loaded extensions: {list(bot.extensions.keys())}")
+    print("---------------------------------")
+
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+bot.run(token=bot.config.token, log_handler=handler)
