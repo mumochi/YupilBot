a738099860d3eddb7d0057c22f89326c5a0143a5
mumochi <149116223+mumochi@users.noreply.github.com>
2025-10-22 13:29:55 -0400

    fix clean_content comparison for message edits

--- ext/listeners.py
+++ ext/listeners.py
@@ -206,7 +206,7 @@ class ListenCog(commands.Cog):
 
     # Run daily checks at EST 12:00/UTC 16:00
     # NOTE: experimental and might also require running fetch_members() instead of calling guild.members
-    @tasks.loop(time=dt.time(hour=17, minute=00, tzinfo=dt.timezone.utc))
+    @tasks.loop(time=dt.time(hour=16, minute=00, tzinfo=dt.timezone.utc))
     async def run_member_checks(self) -> None:
         time_check = dt.datetime.now(dt.timezone.utc) - dt.timedelta(hours=25)
         guild = self.bot.get_guild(int(self.bot.config.server_id))
@@ -294,7 +294,9 @@ class ListenCog(commands.Cog):
         if (timestamp - message.message.created_at).days > MAX_AGE or message.message.author.bot:
             return
         try:
-            if message.cached_message is not None and message.cached_message.content != message.message.content:
+            if message.cached_message is not None:
+                if message.cached_message.clean_content == message.message.clean_content:
+                    returng
                 before = await self.truncate_text(message.cached_message.content)
                 after = await self.truncate_text(message.message.content)
                 user_link = message.cached_message.author.mention
