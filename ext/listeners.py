58f12bdc1cd83cd987dfe162b6b7a3f90939e9e3
mumochi <149116223+mumochi@users.noreply.github.com>
2025-11-15 13:38:03 -0500

    support member/user update checks

--- ext/listeners.py
+++ ext/listeners.py
@@ -200,6 +200,9 @@ class ListenCog(commands.Cog):
     async def on_user_update(self, before: discord.User, after: discord.User) -> None:
         if before.bot:
             return
+        if after.public_flags.spammer:
+            await asyncio.sleep(1) # Help avoid rate-limiting
+            await self.log_spammer(after)
         await asyncio.sleep(1) # Help avoid rate-limiting
         await self.check_excess_dms(after)  
 
@@ -210,6 +213,8 @@ class ListenCog(commands.Cog):
         if after.public_flags.spammer:
             await asyncio.sleep(1) # Help avoid rate-limiting
             await self.log_spammer(after)
+        await asyncio.sleep(1) # Help avoid rate-limiting
+        await self.check_excess_dms(after)          
 
     
     # Listen for voice state changes
